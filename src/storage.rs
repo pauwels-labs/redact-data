366bfe15b94c26db17e2afa7aa0c1fbf3b039a72
Alexandre Pauwels <8890201+ajpauwels@users.noreply.github.com>
2021-05-20 23:33:52 +0100

    tests: add Arc<U> wrapper implementation of DataStorer

--- src/storage.rs
+++ src/storage.rs
@@ -5,6 +5,7 @@ pub mod redact;
 use crate::data::{Data, DataCollection};
 use async_trait::async_trait;
 use error::StorageError;
+use std::{ops::Deref, sync::Arc};
 
 /// The operations a storer of `Data` structs must be able to fulfill.
 #[async_trait]
@@ -22,6 +23,31 @@ pub trait DataStorer: Clone + Send + Sync {
     ) -> Result<DataCollection, StorageError>;
     /// Serializes a piece of `Data` to the the database.
     async fn create(&self, data: Data) -> Result<bool, StorageError>;
+}
+
+/// Allows an `Arc<DataStorer>` to act exactly like a `DataStorer`, dereferencing
+/// itself and passing calls through to the underlying `DataStorer`.
+#[async_trait]
+impl<U> DataStorer for Arc<U>
+where
+    U: DataStorer,
+{
+    async fn get(&self, path: &str) -> Result<Data, StorageError> {
+        self.deref().get(path).await
+    }
+
+    async fn get_collection(
+        &self,
+        path: &str,
+        skip: i64,
+        page_size: i64,
+    ) -> Result<DataCollection, StorageError> {
+        self.deref().get_collection(path, skip, page_size).await
+    }
+
+    async fn create(&self, value: Data) -> Result<bool, StorageError> {
+        self.deref().create(value).await
+    }
 }
 
 pub mod tests {
